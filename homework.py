8b505ed8d1d2c5a7e4d0e0b8c088df9cced9a203
Andrius Sabaliauskas <andrius.sabaliauskass@gmail.com>
2023-03-02 21:01:22 +0200

    adding all

---
+++ homework.py
@@ -0,0 +1,12 @@
+# Ideas:
+# -----------------------------------
+# Kalkuliatorius 
+# Arabiškų skaičių konvertavimas į romėniškus ir atvirkščiai 
+# Boulingo taškų skaičiuoklė 
+# Saugių slaptažodžių generatorius 
+# Klausimynas 
+# Testas 
+# Žaidimai: Kryžiukai/nuliukai,  Akmuo, lapas, žirklės 
+# Bet kokia kita norima programa. 
+# ------------------------------------
+# Requirements: OOP (Inheritance as minimum), functions, modules, import, types, clean code , naming, logging.
